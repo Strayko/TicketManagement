5b29a3a822a0c6347cb7577b4249eafbb06ae815
Strayko <strayko_lol@windowslive.com>
2021-03-28 21:03:50 +0200

    repository implementation and db schema

--- TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
+++ TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -1,9 +1,13 @@
-﻿using TicketManagement.Domain.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TicketManagement.Domain.Entities;
 
 namespace TicketManagement.Application.Contracts.Persistence
 {
     public interface IOrderRepository : IAsyncRepository<Order>
     {
-        
+        Task<List<Order>> GetPagedOrdersForMonth(DateTime date, int page, int size);
+        Task<int> GetTotalCountOfOrdersForMonth(DateTime date);
     }
 }
