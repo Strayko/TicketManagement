7de36786cd271933e9f85b59cc2bafb689ba7875
Strayko <strayko_lol@windowslive.com>
2021-03-28 16:49:01 +0200

    category command, custom exception and fluent validation

--- TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
+++ TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
@@ -1,9 +1,11 @@
-﻿using TicketManagement.Domain.Entities;
+﻿using System;
+using System.Threading.Tasks;
+using TicketManagement.Domain.Entities;
 
 namespace TicketManagement.Application.Contracts.Persistence
 {
     public interface IEventRepository : IAsyncRepository<Event>
     {
-        
+        Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate);
     }
 }
--- TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
 using TicketManagement.Application.Contracts.Persistence;
+using TicketManagement.Application.Exceptions;
 using TicketManagement.Domain.Entities;
 
 namespace TicketManagement.Application.Features.Events.Commands.CreateEvent
@@ -21,6 +22,12 @@ namespace TicketManagement.Application.Features.Events.Commands.CreateEvent
         
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
         {
+            var validator = new CreateEventCommandValidator(_eventRepository);
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.Errors.Count > 0)
+                throw new ValidationException(validationResult);
+
             var @event = _mapper.Map<Event>(request);
             @event = await _eventRepository.AddAsync(@event);
 
