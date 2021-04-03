ab7258aa81f1360ba19612f8e3e4eb15f5352977
Strayko <strayko_lol@windowslive.com>
2021-04-03 20:14:45 +0200

    email service

--- TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -3,8 +3,10 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
+using TicketManagement.Application.Contracts.Infrastracture;
 using TicketManagement.Application.Contracts.Persistence;
 using TicketManagement.Application.Exceptions;
+using TicketManagement.Application.Models.Mail;
 using TicketManagement.Domain.Entities;
 
 namespace TicketManagement.Application.Features.Events.Commands.CreateEvent
@@ -13,13 +15,15 @@ namespace TicketManagement.Application.Features.Events.Commands.CreateEvent
     {
         private readonly IMapper _mapper;
         private readonly IEventRepository _eventRepository;
+        private readonly IEmailService _emailService;
 
-        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository)
+        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository, IEmailService emailService)
         {
             _mapper = mapper;
             _eventRepository = eventRepository;
+            _emailService = emailService;
         }
-        
+
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
         {
             var validator = new CreateEventCommandValidator(_eventRepository);
@@ -30,6 +34,22 @@ namespace TicketManagement.Application.Features.Events.Commands.CreateEvent
 
             var @event = _mapper.Map<Event>(request);
             @event = await _eventRepository.AddAsync(@event);
+
+            var email = new Email()
+            {
+                To = "example@email.com", Body = $"A new event was created: {request}",
+                Subject = "A new event was created"
+            };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
 
             return @event.EventId;
         }
---
+++ TicketManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TicketManagement.Application.Contracts.Infrastracture;
+using TicketManagement.Application.Models.Mail;
+using TicketManagement.Infrastructure.Mail;
+
+namespace TicketManagement.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
