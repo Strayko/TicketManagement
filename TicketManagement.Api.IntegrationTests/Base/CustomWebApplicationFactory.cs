13e657ce25b2e1f603572d0c7313f5a56806b472
Strayko <strayko_lol@windowslive.com>
2021-04-21 22:32:26 +0200

    dispose factory client

--- TicketManagement.Api.IntegrationTests/Base/CustomWebApplicationFactory.cs
+++ TicketManagement.Api.IntegrationTests/Base/CustomWebApplicationFactory.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Linq;
 using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using TicketManagement.Persistence;
 
@@ -15,13 +19,25 @@ namespace TicketManagement.Api.IntegrationTests.Base
         {
             builder.ConfigureServices(services =>
             {
+                services.AddHttpContextAccessor();
+
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<TicketManagementDbContext>));
+
+                services.Remove(descriptor);
+
+                services.AddHttpsRedirection(options =>
+                {
+                    options.HttpsPort = 5001;
+                });
+
                 services.AddDbContext<TicketManagementDbContext>(options =>
                 {
                     options.UseInMemoryDatabase("TicketManagementDbContextInMemoryTest");
                 });
-
+                
                 var sp = services.BuildServiceProvider();
-
+                
                 using (var scope = sp.CreateScope())
                 {
                     var scopedServices = scope.ServiceProvider;
@@ -29,7 +45,8 @@ namespace TicketManagement.Api.IntegrationTests.Base
                     var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
 
                     context.Database.EnsureCreated();
-
+                    Console.WriteLine(context);
+                    
                     try
                     {
                         Utilities.InitializeDbForTests(context);
--- TicketManagement.Api.IntegrationTests/Controllers/CategoryControllerTests.cs
+++ TicketManagement.Api.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -1,7 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using TicketManagement.Api.IntegrationTests.Base;
+using TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using Xunit;
+
 namespace TicketManagement.Api.IntegrationTests.Controllers
 {
-    public class CategoryControllerTests
+    [TestFixture]
+    public class CategoryControllerTests : IDisposable
     {
+        private CustomWebApplicationFactory<Startup> _factory;
         
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _factory = new CustomWebApplicationFactory<Startup>();
+        }
+
+        [Test]
+        public async Task ReturnSuccessResult()
+        {
+            var client = _factory.GetAnonymousClient();
+
+            var response = await client.GetAsync("/api/category/all");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<List<CategoryListVm>>(responseString);
+            Console.WriteLine(result);
+
+            Assert.IsNotEmpty(result);
+            
+        }
+
+        public void Dispose()
+        {
+            _factory.Dispose();
+        }
     }
 }
